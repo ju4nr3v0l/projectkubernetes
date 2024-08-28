cbde31826607327fd002a0d0ab91101a7ab10159
ju4nr3v0l <jadamavecool@gmail.com>
2024-08-27 23:40:57 -0500

    feat[add final integration with argocd an kubernetes]: add final integration with argocd an kubernetes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from grupo2diplomado con un cambio de tag!');
+    res.send('Hello from grupo2diplomado con el cambio final para que escuche argocd por fin!!');
 });
 
 app.listen(port, () => {
